d782c8597f25c1c18a68dab1dbe411139e021453
arnaud <arnaud@dragsystem.local>
2012-01-31 10:04:08 +0100

    Course built 3 - to check whether userIsIntheMiddleOfTypingANumber works

--- CalculatorGit/CalculatorViewController.m
+++ CalculatorGit/CalculatorViewController.m
@@ -8,12 +8,24 @@
 
 #import "CalculatorViewController.h"
 
+@interface CalculatorViewController ()
+/*  declaration of private instance variables
+ */
+/*  boolean, which is either YES or NO
+    long variable names is encouraged, Xcode helps typing
+ */
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+
+@end
+
 @implementation CalculatorViewController
 /*  add the name of the instance variable (_display):
     @synthesize creates the setter and the getter
     when the storyboard gets loaded by the stroryboard calles the setter
  */
 @synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+
 /*  All original methods in the template have been deleted 
  */
 
@@ -31,6 +43,8 @@
 /*  %@ is for strings 
     NSLog(@"digit pressed = %@", digit);
  */
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        
     self.display.text = [self.display.text stringByAppendingString:digit];
 /*  which is short for:
     UILabel *myDisplay = self.display; // the same as: [self display]
@@ -38,6 +52,15 @@
     NSString *newText = [currentText stringByAppendingString:digit]; // add a string to another string
     myDisplay.text = newText; // use the setter for this:[myDisplay setText:newText];
  */
+    }
+    else { // the user starts off a new number
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+}
+- (IBAction)operationPressed:(id)sender {
+}
+- (IBAction)enterPressed {
 }
 
 @end
